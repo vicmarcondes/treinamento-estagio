346be5f4ff2cf5695b126543091a231b563a3efc
Victor Moreno <victorgalves@hotmail.com>
2020-01-14 15:32:52 -0300

    Add footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
