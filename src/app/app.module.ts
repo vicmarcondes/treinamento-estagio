8905cc258664479f468716a7722ceb201741d0c2
Victor Moreno <victorgalves@hotmail.com>
2020-01-14 12:12:13 -0300

    Add Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule
